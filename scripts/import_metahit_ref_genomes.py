8a55a22666cba976b5e9b6495501e23f3efab50e
pajanne <pajanne@gmail.com>
2011-08-22 19:21:22 +0100

    import improved metahit genome sequences to /nfs/pathogen/metahit/refs/

--- scripts/import_metahit_ref_genomes.py
+++ scripts/import_metahit_ref_genomes.py
@@ -8,6 +8,7 @@ Copyright (c) 2011 Wellcome Trust Sanger Institute. All rights reserved.
 
 from optparse import OptionParser
 import sys, os
+import shutil
 
 ### ---------------------------------------------------------------------------
 def main():
@@ -27,11 +28,29 @@ def main():
         if line[0] == '!':
             continue
         if line.strip():
-            fasta_file = line.strip()
-            if not os.path.exists(fasta_file):
-                print "NOT FOUND %s" % fasta_file
-            else:
-                print "FOUND     %s" % fasta_file
+            line = line.strip()
+            if line.count('||') > 0:
+                # !common_name||genus||species_strain||path_to_fasta_file
+                list = line.split('||')
+                common_name = list[0]
+                genus = list[1]
+                species = list[2]
+                new_fasta_file_path = "/nfs/pathogen/metahit/refs/%s/%s/improved/" % (genus, species)
+                new_fasta_file_name = "%s_%s.fasta" % (genus, species)
+                fasta_file = list[3]
+                
+                if not os.path.exists(fasta_file):
+                    print "NOT FOUND %s" % fasta_file
+                else:
+                    if not os.path.isfile(fasta_file):
+                        print "NO FILE   %s" % fasta_file
+                    else:
+                        print "FOUND     %s" % fasta_file
+                        new_fasta_file = "%s/%s" % (new_fasta_file_path, new_fasta_file_name)
+                        print "Copying %s to %s" % (fasta_file, new_fasta_file)
+                        if not os.path.exists(new_fasta_file_path):
+                            os.makedirs(new_fasta_file_path)
+                        shutil.copyfile(fasta_file, new_fasta_file)
 
 ### ---------------------------------------------------------------------------
 if __name__ == '__main__':
